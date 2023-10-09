a5a21b46048f6cf8426e4006b745f07a84f11aa9
IgorMushk <igor.mushketik@gmail.com>
2023-10-09 12:10:48 +0300

    Add componets  Route & Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import About from "../pages/About";
+import Home from "../pages/Home";
+import Products from "../pages/Products";
+import NotFound from "pages/NotFound";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template!
-    </div>
+<div>
+  <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/about' element={<About/>} />
+    <Route path='/products' element={<Products/>}/>
+    <Route path='*' element={<NotFound/>}/>
+  </Routes>
+</div>
   );
 };
