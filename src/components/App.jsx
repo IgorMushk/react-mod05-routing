54fc19a8388821156b6c5f293391afd1b652ba6c
IgorMushk <igor.mushketik@gmail.com>
2023-10-09 12:17:05 +0300

    Add componets Link & NavLink

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import About from "../pages/About";
 import Home from "../pages/Home";
 import Products from "../pages/Products";
@@ -7,6 +7,12 @@ import NotFound from "pages/NotFound";
 export const App = () => {
   return (
 <div>
+    <nav>
+      <Link to='/'>Home </Link>
+      <Link to='/about'>About </Link>
+      <Link to='/produts'>Products </Link>
+    </nav>
+
   <Routes>
     <Route path='/' element={<Home/>} />
     <Route path='/about' element={<About/>} />
