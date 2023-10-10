0b023e201f9c66c9dcca0a6eb740af9e9c8ad2e4
IgorMushk <igor.mushketik@gmail.com>
2023-10-10 12:31:48 +0300

    Location object - useLocation -  location.stte - BACK TO PRODUCTS - Upd ProductList & ProductDetails. + BackLink.jsx
    Модуль 5 - Занятие 10 - Маршрутизация часть 2 Объект местоположения
    
    https://www.edu.goit.global/ru/learn/10292928/31183/31257/textbook
    
    Каждая запись в стеке истории навигации описана объектом местоположения (location) со стандартным набором свойств, которые хранят полную информацию о URL. Когда пользователь нажимает на ссылки и перемещается по приложению, текущее местоположение меняется и добавляется новая запись истории.
    
    {
      pathname: string;
      search: string;
      hash: string;
      state: any;
      key: string;
    }
    
    pathname - строка, содержащая часть URL от начального / и до символа ?.
    search - содержит всю строку запроса. Если параметры отсутствуют, значением будет пустая строка.
    hash - строка, содержащая часть URL от конца строки запроса и символа #, за которым следует идентификатор фрагмента URL-адреса. Если идентификатор фрагмента отсутствует, значением будет пустая строка.
    state - произвольное значение, которое содержит дополнительную информацию, связанную с местоположением, но не отображается в URL-адресе. Задается разработчиком. Используется для передачи данных между маршрутами.
    key - уникальная строка идентификатор, связанная с этим местоположением. Служебное свойство, значение которого задается автоматически для каждой новой записи в истории навигации.
    Например, для такого URL объект местоположения будет выглядеть следующим образом.
    
    // https://gomerch.it/products?name=hoodie&color=orange&maxPrice=500#agreement
    
    {
      "pathname": "/products",
      "search": "?name=hoodie&color=orange&maxPrice=500",
      "hash": "#agreement",
      "state": null,
      "key": "random-browser-generated-id"
    }

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { CardWrapper, Container, ProductName, } from './ProductList.styled';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const ProductList = ({ products }) => {
+const location = useLocation();
+
   return (
     <Container>
       {products.map(product => (
         <CardWrapper key={product.id}>
-          <Link to={`${product.id}`}>
+          <Link to={`${product.id}`} state={{from: location}}>
             <img src="https://via.placeholder.com/200x100" alt="" />
             <ProductName>{product.name}</ProductName>
           </Link>
